a663cb11ea0514eae01ce1e211601f23d4264092
KBence210 <79148226+KBence210@users.noreply.github.com>
2022-03-28 13:04:25 +0200

    Add files via upload

---
+++ 10.gyak/1_bankszamla_kivetelkezeles/CheckingAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_bankszamla_kivetelkezeles
+{
+    class CheckingAccount
+    {
+        private double balance;
+        private int number;
+
+        public CheckingAccount(int number)
+        {
+            this.number = number;
+        }
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+            }
+            else
+            {
+                double needs = amount - balance;
+                throw new InsufficientFundsException(needs);
+            }
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public int GetNumber()
+        {
+            return number;
+        }
+
+    }
+}
